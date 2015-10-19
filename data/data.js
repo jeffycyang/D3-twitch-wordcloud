68dc0a87dfd616c8ad581c2a5b12f81a09ff606b
MKS-students <mks.students@hackreactor.com>
2015-10-19 16:13:11 -0700

    First example with D3 word cloud

---
+++ data/data.js
@@ -0,0 +1,229 @@
+var data = {
+    "_total": 1103,
+    "_links": {
+        "self": "https://api.twitch.tv/kraken/games/top?limit=10&offset=0",
+        "next": "https://api.twitch.tv/kraken/games/top?limit=10&offset=10"
+    },
+    "top": [
+        {
+            "viewers": 126360,
+            "channels": 1850,
+            "game": {
+                "name": "League of Legends",
+                "_id": 21779,
+                "giantbomb_id": 24024,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 82635,
+            "channels": 1090,
+            "game": {
+                "name": "Counter-Strike: Global Offensive",
+                "_id": 32399,
+                "giantbomb_id": 36113,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/Counter-Strike:%20Global%20Offensive-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/Counter-Strike:%20Global%20Offensive-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/Counter-Strike:%20Global%20Offensive-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/Counter-Strike:%20Global%20Offensive-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 69771,
+            "channels": 335,
+            "game": {
+                "name": "Hearthstone: Heroes of Warcraft",
+                "_id": 138585,
+                "giantbomb_id": 42033,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/Hearthstone:%20Heroes%20of%20Warcraft-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/Hearthstone:%20Heroes%20of%20Warcraft-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/Hearthstone:%20Heroes%20of%20Warcraft-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/Hearthstone:%20Heroes%20of%20Warcraft-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/Hearthstone:%20Heroes%20of%20Warcraft-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/Hearthstone:%20Heroes%20of%20Warcraft-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/Hearthstone:%20Heroes%20of%20Warcraft-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/Hearthstone:%20Heroes%20of%20Warcraft-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 27952,
+            "channels": 139,
+            "game": {
+                "name": "ArmA III",
+                "_id": 31750,
+                "giantbomb_id": 35383,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/ArmA%20III-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/ArmA%20III-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/ArmA%20III-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/ArmA%20III-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/ArmA%20III-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/ArmA%20III-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/ArmA%20III-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/ArmA%20III-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 21171,
+            "channels": 377,
+            "game": {
+                "name": "H1Z1",
+                "_id": 417892,
+                "giantbomb_id": 45959,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/H1Z1-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/H1Z1-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/H1Z1-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/H1Z1-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/H1Z1-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/H1Z1-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/H1Z1-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/H1Z1-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 18595,
+            "channels": 374,
+            "game": {
+                "name": "Dota 2",
+                "_id": 29595,
+                "giantbomb_id": 32887,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/Dota%202-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/Dota%202-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/Dota%202-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/Dota%202-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/Dota%202-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/Dota%202-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/Dota%202-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/Dota%202-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 18012,
+            "channels": 1144,
+            "game": {
+                "name": "Destiny",
+                "_id": 280721,
+                "giantbomb_id": 36067,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/Destiny-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/Destiny-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/Destiny-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/Destiny-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/Destiny-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/Destiny-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/Destiny-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/Destiny-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 16985,
+            "channels": 522,
+            "game": {
+                "name": "FIFA 16",
+                "_id": 489608,
+                "giantbomb_id": 49601,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/FIFA%2016-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/FIFA%2016-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/FIFA%2016-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/FIFA%2016-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/FIFA%2016-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/FIFA%2016-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/FIFA%2016-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/FIFA%2016-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 13759,
+            "channels": 99,
+            "game": {
+                "name": "Blade and Soul",
+                "_id": 20423,
+                "giantbomb_id": 22516,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/Blade%20and%20Soul-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/Blade%20and%20Soul-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/Blade%20and%20Soul-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/Blade%20and%20Soul-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/Blade%20and%20Soul-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/Blade%20and%20Soul-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/Blade%20and%20Soul-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/Blade%20and%20Soul-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        },
+        {
+            "viewers": 10670,
+            "channels": 49,
+            "game": {
+                "name": "Gaming Talk Shows",
+                "_id": 417752,
+                "giantbomb_id": 0,
+                "box": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-boxart/Gaming%20Talk%20Shows-272x380.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-boxart/Gaming%20Talk%20Shows-136x190.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-boxart/Gaming%20Talk%20Shows-52x72.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-boxart/Gaming%20Talk%20Shows-{width}x{height}.jpg"
+                },
+                "logo": {
+                    "large": "http://static-cdn.jtvnw.net/ttv-logoart/Gaming%20Talk%20Shows-240x144.jpg",
+                    "medium": "http://static-cdn.jtvnw.net/ttv-logoart/Gaming%20Talk%20Shows-120x72.jpg",
+                    "small": "http://static-cdn.jtvnw.net/ttv-logoart/Gaming%20Talk%20Shows-60x36.jpg",
+                    "template": "http://static-cdn.jtvnw.net/ttv-logoart/Gaming%20Talk%20Shows-{width}x{height}.jpg"
+                },
+                "_links": {}
+            }
+        }
+    ]
+}
